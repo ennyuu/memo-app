ecc693468f9a5fec3cac7ae08d09c04c4bb5b230
Yuki Endo <yuki.endo.2487@gmail.com>
2025-03-08 15:24:59 +0900

    Indexコンポーネントのヘッダーをスタイル追加とレイアウトの改善

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,12 +3,14 @@ import { View, Text, StyleSheet } from "react-native";
 const Index = ():JSX.Element => {
     return (
         <View style={styles.container}>
-            <View>
-                <View>
-                    <Text>Memo App</Text>
-                    <Text>ログアウト</Text>
+
+            <View style={styles.header}>
+                <View style={styles.headerInner}>
+                    <Text style={styles.headerTitle}>Memo App</Text>
+                    <Text style={styles.headerRight}>ログアウト</Text>
                 </View>
             </View>
+
             <View>
 
                 <View>
@@ -53,9 +55,30 @@ const Index = ():JSX.Element => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        backgroundColor: '#ffffff',
+    },
+    header: {
+        height: 104,
+        backgroundColor: '#467FD3',
+        justifyContent: 'flex-end',
+    },
+    headerInner: {
         alignItems: 'center',
-    }
+    },
+    headerTitle: {
+        marginBottom: 8,
+        fontSize: 22,
+        lineHeight: 32,
+        color: '#ffffff',
+        fontWeight: 'bold',
+    },
+    headerRight: {
+        position: 'absolute',
+        right: 16,
+        bottom: 16,
+        color: 'rgba(255, 255, 255, 0.75)',
+    },
+
 })
 
 export default Index;
