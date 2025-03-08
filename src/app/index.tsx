98e325896ca172920c81dbb1ea756ca249b92281
Yuki Endo <yuki.endo.2487@gmail.com>
2025-03-08 21:52:06 +0900

    Indexコンポーネントの円形ボタンスタイルを追加し、レイアウトを改善

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -45,8 +45,8 @@ const Index = ():JSX.Element => {
 
             </View>
 
-            <View>
-                <Text>+</Text>
+            <View style={styles.circleButton}>
+                <Text style={styles.circleButtonLabel}>+</Text>
             </View>
         </View>
     );
@@ -96,6 +96,27 @@ const styles = StyleSheet.create({
         fontSize: 12,
         lineHeight: 16,
         color: '#848484',
+    },
+    circleButton: {
+        backgroundColor: '#467FD3',
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        right: 40,
+        bottom: 40,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 8 },
+        shadowOpacity: 0.25,
+        shadowRadius: 8,
+        elevation: 8,
+    },
+    circleButtonLabel: {
+        color: '#ffffff',
+        fontSize: 40,
+        lineHeight: 48,
     },
 
 })
