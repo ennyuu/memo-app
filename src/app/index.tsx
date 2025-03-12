490934ba6796bdfd35e9d39b6c65d6e68cc8a96b
Yuki Endo <yuki.endo.2487@gmail.com>
2025-03-12 22:27:17 +0900

    Firebase認証の状態に基づいてリダイレクトを実装

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,20 @@
-import { Redirect } from "expo-router";
+import { Redirect, router } from "expo-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+
+import { auth } from "../config";
 
 const Index = (): JSX.Element => {
+useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+        if (user !== null) {
+            router.replace('/memo/list');
+        } else {
+            router.replace('/auth/login');
+        }
+    });
+}, []);
+
     return <Redirect href="auth/login" />
 }
 
