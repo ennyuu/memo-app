238f5682b9921d28727621eb215c9a4a507c0e21
Yuki Endo <yuki.endo.2487@gmail.com>
2025-03-15 23:35:25 +0900

    メモリストアイテムに削除機能を追加し、ユーザー確認のアラートを実装

--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -1,12 +1,33 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
 import { Link } from "expo-router";
+import {deleteDoc, doc } from 'firebase/firestore'
 
 import Icon from "./Icom";
 import { type Memo } from "../../types/memo";
+import { auth, db } from "../config";
 
 interface Props {
     memo: Memo;
 }
+
+const handlePress = (id: string): void => {
+    if (auth.currentUser === null) { return }
+    const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id);
+    Alert.alert('メモを削除します', 'よろしいですか？', [
+        {
+            text: 'キャンセル',
+        },
+        {
+            text: '削除する',
+            style: 'destructive',
+            onPress: () => {
+                deleteDoc(ref)
+                .catch(() => { Alert.alert('削除に失敗しましました')})
+            },
+        },
+    ])
+}
+
 const MemoListItem = (props: Props): JSX.Element | null => {
     const { memo } = props;
     const { bodyText, createdAt } = memo;
@@ -27,7 +48,7 @@ const MemoListItem = (props: Props): JSX.Element | null => {
                     <Text style={styles.memoListDate}>{dateString}</Text>
                 </View>
 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => { handlePress(memo.id) }}>
                     <Icon name="delete" size={24} color="#B0B0B0" />
                 </TouchableOpacity>
 
